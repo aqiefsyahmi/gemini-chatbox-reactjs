86a45fee59d580cba36d687b60136c0fbb2aeaf0
KYUCODE SOLUTION <104400612+aqiefsyahmi@users.noreply.github.com>
2025-06-06 12:22:30 +0800

    all needed file for the project

---
+++ src/components/Message.jsx
@@ -0,0 +1,20 @@
+const Message = ({ message }) => {
+  return (
+    <div
+      id={message.id}
+      className={`message ${message.role}-message ${
+        message.loading ? "loading" : ""
+      } ${message.error ? "error" : ""}`}
+    >
+      {message.role === "bot" && (
+        <img
+          className="avatar"
+          src="gemini-chatbot-logo.svg"
+          alt="Bot Avatar"
+        />
+      )}
+      <p className="text">{message.content}</p>
+    </div>
+  );
+};
+export default Message;
